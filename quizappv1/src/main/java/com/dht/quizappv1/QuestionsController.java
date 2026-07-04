2ffec9afd64927f67d7a0d387b405e9693d3e6eb
unknown <tiendatdmx2006@gmail.com>
2026-07-04 16:25:42 +0700

    new

---
+++ quizappv1/src/main/java/com/dht/quizappv1/QuestionsController.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.dht.quizappv1;
+
+import com.dht.pojo.Category;
+import com.dht.pojo.Choice;
+import com.dht.pojo.Level;
+import com.dht.pojo.Question;
+import com.dht.services.CategoryServices;
+import com.dht.services.questions.QuestionServices;
+import com.dht.utils.Configs;
+import com.dht.utils.MyAlertSingleton;
+import com.dht.utils.MyConnectionSingleton;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author admin
+ */
+public class QuestionsController implements Initializable {
+    @FXML private ComboBox<Category> cbCates;
+    @FXML private ComboBox<Level> cbLevels;
+    @FXML private TableView<Question> tvQuestions;
+    @FXML private VBox vChoice;
+    @FXML private TextArea txtContent;
+    
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.loadColums();
+        try {
+            this.cbCates.setItems(FXCollections.observableList(Configs.cateServices.getCates()));
+            this.cbLevels.setItems(FXCollections.observableList(Configs.levelServices.geLevel()));
+            this.tvQuestions.setItems(FXCollections.observableList(Configs.quesServices.getQuestions()));
+        } catch (SQLException ex) {
+//            Logger.getLogger(QuestionsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+    
+    private void loadColums() {
+        TableColumn colId = new TableColumn("Id");
+        colId.setCellValueFactory(new PropertyValueFactory("id"));
+        
+        TableColumn colContent = new TableColumn("Nội dung câu hỏi");
+        colContent.setCellValueFactory(new PropertyValueFactory("content"));
+        colContent.setPrefWidth(300);
+        
+        this.tvQuestions.getColumns().addAll(colId, colContent);
+    }
+    
+    public void addChoice(ActionEvent e){
+        HBox h = new HBox();
+        h.getStyleClass().add("Container");
+        
+        RadioButton r = new RadioButton();
+        TextField t = new TextField();
+        h.getChildren().addAll(r, t);
+        //t.getStyleClass().add("Subject");
+        
+        this.vChoice.getChildren().add(h);
+   }
+    
+    public void addQuestion(ActionEvent e){
+        Question q = new Question.Builder().setCategory(this.cbCates.getSelectionModel().getSelectedItem())
+                .setContent(this.txtContent.getText())
+                .setLevel(this.cbLevels.getSelectionModel().getSelectedItem()).build();
+        
+        List<Choice> choices = new ArrayList<>();
+        for(var hb: this.vChoice.getChildren()){
+            HBox h = (HBox) hb;
+            
+            RadioButton r = (RadioButton) h.getChildren().get(0);
+            TextField t = (TextField) h.getChildren().get(1);
+            choices.add(new Choice(t.getText(), r.isSelected()));
+        }
+        
+        try {
+            Configs.uQuesServices.addQuestion(q, choices);
+            
+            MyAlertSingleton.getInstance().showMsg("Them cau hoi thanh cong");
+        } catch (SQLException ex) {
+            MyAlertSingleton.getInstance().showMsg("Them cau hoi that bai, ly do" + ex.getMessage());
+        }
+    }
+}
---
+++ quizappv1/src/main/java/com/dht/services/questions/updateQuestionServices.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dht.services.questions;
+
+import com.dht.pojo.Choice;
+import com.dht.pojo.Question;
+import com.dht.utils.MyConnectionSingleton;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author admin
+ */
+public class updateQuestionServices {
+    public void addQuestion(Question q, List<Choice> choices) throws SQLException {
+        Connection conn = MyConnectionSingleton.getInstance().connect();
+        conn.setAutoCommit(false);
+        String sql = "Insert INTO question(content, category_id, level_id) Values(?, ?, ?)";
+        PreparedStatement stm = conn.prepareCall(sql);
+        stm.setString(1, q.getContent());
+        stm.setInt(2, q.getCategory().getId());
+        stm.setInt(3, q.getLevel().getId());
+        
+        if(stm.executeUpdate() > 0){
+            ResultSet r = stm.getGeneratedKeys();
+            if(r.next()){
+                int qID = r.getInt(1);
+                if( qID > 0){
+                    sql = "Insert INTO choice(content, is_correct, question_id) Values(?, ?, ?)";
+                    stm= conn.prepareCall(sql);
+                    
+                    for (var c : choices){
+                        stm.setString(1, c.getContent());
+                        stm.setBoolean(2, c.isCorrect());
+                        stm.setInt(3, qID);
+                        stm.executeUpdate();
+                    }
+                    
+                    conn.commit();
+                }
+            }
+        }
+    }
+}
